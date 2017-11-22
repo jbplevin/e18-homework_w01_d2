e0713ef67966a05bbc5f24721d98079e8b255f43
Joe Plevin <joe.plevin@hotmail.co.uk>
2017-11-22 10:10:04 +0000

    added subtract function to my_functions.rb

--- my_functions.rb
+++ my_functions.rb
@@ -1,10 +1,6 @@
-def greet(name, time_of_day)
-  words = "Hello #{name}, good #{time_of_day}"
-  return words
-end
-name = "Joe"
-p greet(name, "afternoon")
 def add(first_number,second_number)
   return first_number + second_number
 end
-p add(2,3)
+def subtract(first_number,second_number)
+  return first_number - second_number
+end
